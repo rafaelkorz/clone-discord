0e095046f7837502bc80f0828aaf9db79e1c36ec
Rafael Korz <rafael.korz@gmail.com>
2020-06-23 21:30:40 -0300

    Created components Layout and ServerList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Layout from './components/Layout';
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
   return (
-    <h1>Rafael korz</h1>
+    <>
+      <Layout />
+
+      <GlobalStyles />
+    </>
   );
 }
 
 export default App;
+
