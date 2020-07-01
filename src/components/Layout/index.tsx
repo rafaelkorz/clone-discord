896261513ea0ca11ae186c067f87db261a3d9c44
Rafael Korz <rafael.korz@gmail.com>
2020-07-01 19:51:21 -0300

    Finished

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -7,6 +7,8 @@ import ServerName from '../ServerName';
 import ChannelInfo from '../ChannelInfo';
 import ChannelList from '../ChannelList';
 import UserInfo from '../UserInfo';
+import UserList from '../userList';
+import ChannelData from '../ChannelData';
 
 const Layout: React.FC = () => {
   return ( 
@@ -15,9 +17,12 @@ const Layout: React.FC = () => {
       <ServerList />
       <ChannelInfo />
       <ChannelList />    
+      <ChannelData />
       <UserInfo />  
+      <UserList />
     </Grid>
   )
 };
 
 export default Layout;
+
